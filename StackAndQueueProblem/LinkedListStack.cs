b0943c0bb2057fe44086f8ff584221410cc45031
Anilrocky <anilkumarac96@gmail.com>
2023-03-22 20:08:55 +0530

    [Anil] Add:Push elements 56,30,70 to Stack

---
+++ StackAndQueueProblem/LinkedListStack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueProblem
+{
+    public class LinkedListStack
+    {
+        public Node top;
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} is pushed to stack", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- StackAndQueueProblem/Program.cs
+++ StackAndQueueProblem/Program.cs
@@ -5,7 +5,27 @@ namespace StackAndQueueProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome");
+            bool flag = true;
+            LinkedListStack stack = new LinkedListStack();
+            while (flag)
+            {
+                Console.WriteLine("\nChoose option to perform \n1.Push elements to Stack \n2.Display \n3.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        stack.Push(70);
+                        stack.Push(30);
+                        stack.Push(56);
+                        break;
+                    case 2:
+                        stack.Display();
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }

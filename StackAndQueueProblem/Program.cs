d9711f7ab1b6aa43095164971a430d7c6163cc4d
Anilrocky <anilkumarac96@gmail.com>
2023-03-22 20:00:49 +0530

    [Anil] Add:Folder Structure Created

---
+++ StackAndQueueProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace StackAndQueueProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+        }
+    }
+}
+
